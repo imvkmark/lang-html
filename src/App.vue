6b140e290c3764e5adf2f5c457b1c883f665f5f3
duoli <zhaody901@126.com>
2021-01-09 11:16:46 +0800

    伙玩的 Demo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
     <div id="app" class="container">
         <side/>
-        <router-view/>
+        <div class="content">
+            <router-view/>
+        </div>
     </div>
 </template>
 
--- src/component/side.vue
+++ src/component/side.vue
@@ -1,16 +1,18 @@
 <template>
-    <ul>
-        <li>
-            <router-link :to="{ name: 'align'}">Align</router-link>
-        </li>
-        <li>
-            <router-link :to="{ name: 'form'}">Form</router-link>
-        </li>
-    </ul>
+    <div class="side">
+        <ul>
+            <li>
+                <router-link :to="{ name: 'align' }">Align</router-link>
+            </li>
+            <li>
+                <router-link :to="{ name: 'form' }">Form</router-link>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
 export default {
-    name : 'Side'
+    name : "Side"
 };
 </script>
